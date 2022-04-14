918bf5a819d5d53a4c4256c7f8fd361e6e99194f
MoBosse <50657629+MoBosse@users.noreply.github.com>
2022-04-14 11:34:36 +0000

    Deploying to gh-pages from  @ 85357f6c9d2afaa27c826c0b17510f36fc309257 🚀

---
+++ assets/js/pdf.js
@@ -0,0 +1,7 @@
+opensdg.createPdf = function () {
+
+    var doc = new jsPDF();
+    //doc.text(document.getElementById("input").value, 10, 10);
+    doc.text("Hello world!", 10, 10);
+    doc.save("output.pdf");
+};
