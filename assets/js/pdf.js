306285bbf28b9e6ccd295bdc8a6bf4946fd7831f
MoBosse <50657629+MoBosse@users.noreply.github.com>
2022-04-14 13:19:52 +0000

    Deploying to gh-pages from  @ 423a227b5ceeb01ae30facb468a0b90e61c5f462 🚀

--- assets/js/pdf.js
+++ assets/js/pdf.js
@@ -1,27 +1,27 @@
 opensdg.createPdf = function () {
 
-    // var doc = new jsPDF();
-    // //doc.text(document.getElementById("input").value, 10, 10);
-    // //doc.text(content, 10, 10);
-    // doc.text("Text goes here",10,10)
-    // doc.save("output.pdf");
-
-
-    var elementHandler = {
-      '#ignorePDF': function (element, renderer) {
-        return true;
-      }
-    };
-    var source = window.document.getElementsById("pdfContent")[0];
-    doc.fromHTML(
-        source,
-        15,
-        15,
-        {
-          'width': 180,'elementHandlers': elementHandler
-        });
-
+    var doc = new jsPDF();
+    //doc.text(document.getElementById("input").value, 10, 10);
+    //doc.text(content, 10, 10);
+    doc.text("Text goes here",10,10);
     doc.save("output.pdf");
+
+
+    // var elementHandler = {
+    //   '#ignorePDF': function (element, renderer) {
+    //     return true;
+    //   }
+    // };
+    // var source = window.document.getElementsById("pdfContent")[0];
+    // doc.fromHTML(
+    //     source,
+    //     15,
+    //     15,
+    //     {
+    //       'width': 180,'elementHandlers': elementHandler
+    //     });
+    //
+    // doc.save("output.pdf");
 
 
 
