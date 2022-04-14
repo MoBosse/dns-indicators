98362f16e0baf165e81c3b7948088b3f4a2de8e6
MoBosse <moritz.bosse@destatis.de>
2022-04-14 15:32:54 +0200

    ????

--- assets/js/pdf.js
+++ assets/js/pdf.js
@@ -1,29 +1,8 @@
-opensdg.createPdf = function () {
+opensdg.createPdf = function (content) {
 
     var doc = new jsPDF();
     //doc.text(document.getElementById("input").value, 10, 10);
-    //doc.text(content, 10, 10);
-    doc.text("Text goes here",10,10);
+    doc.text(content, 10, 10);
     doc.save("output.pdf");
-
-
-    // var elementHandler = {
-    //   '#ignorePDF': function (element, renderer) {
-    //     return true;
-    //   }
-    // };
-    // var source = window.document.getElementsById("pdfContent")[0];
-    // doc.fromHTML(
-    //     source,
-    //     15,
-    //     15,
-    //     {
-    //       'width': 180,'elementHandlers': elementHandler
-    //     });
-    //
-    // doc.save("output.pdf");
-
-
-
 };
 ​
