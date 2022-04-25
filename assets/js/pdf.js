a4b1e4574c6cfc1c0b540f6e34fecb91d03c4713
MoBosse <moritz.bosse@destatis.de>
2022-04-25 09:11:36 +0200

    2.7.1

--- assets/js/pdf.js
+++ assets/js/pdf.js
@@ -1,11 +1,30 @@
 opensdg.createPdf = function () {
 
-    var doc = new jsPDF();
+    var doc = new jsPDF('p', 'pt', 'letter');
 
     source = $('#pdf_content')[0];
-    doc.fromHTML(source);
-    doc.save("output2.pdf");
+    specialElementHandlers = {
+      '#bypassme': function (element, renderer){
+        return true
+      }
+    };
+    margins = {
+      top: 80,
+      bottom: 60,
+      left: 40,
+      width: 522,
+    };
 
+    doc.fromHTML(
+      source,
+      margins.left,
+      margins.top, {
+        'width': margins.width,
+        'elementHandlers': specialElementHandlers
+      },
+    function(dispoae){
+      doc.save('Test.pdf');
+    }, margins);
     //doc.text(content, 10, 10);
     //doc.save("output.pdf");
 };
